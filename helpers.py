dff215b31435d64ec4106bab9ca3de0be8cfedce
Ieva Sataite <ieva.sataite89@gmail.com>
2022-08-29 10:47:46 +0200

    Documented helper functions

--- helpers.py
+++ helpers.py
@@ -3,6 +3,9 @@ from constants import base_url, Colors
 
 
 def get_currencies():
+    """
+    Function to get a list of all the symbols
+    """
     endpoint = 'symbols'
     url = base_url + endpoint
     data = requests.get(url).json()
@@ -10,6 +13,9 @@ def get_currencies():
 
 
 def print_currencies(currencies):
+    """
+    Function to print a list of symbols and currency names
+    """
     for key, value in currencies.items():
         name = value['description']
         print(f'{Colors.bold}{key}{Colors.reset} - {name}')
@@ -17,6 +23,10 @@ def print_currencies(currencies):
 
 
 def get_exchange_rate_or_convert(base, currency, amount=1.00):
+    """
+    Function to convert from one currency to another if amount is provided,
+    otherwise get exchange rate only
+    """
     endpoint = f'convert?from={base}&to={currency}'
     url = base_url + endpoint
     data = requests.get(url).json()
@@ -31,6 +41,9 @@ def get_exchange_rate_or_convert(base, currency, amount=1.00):
 
 
 def check_code(code, currencies):
+    """
+    Helper function to validate currency code as a command
+    """
     if code in currencies:
         return code
     print(f'{Colors.errors}Invalid currency code. Please try again.{Colors.reset}')
@@ -38,6 +51,9 @@ def check_code(code, currencies):
 
 
 def check_amount(amount):
+    """
+    Helper function to validate amount entered
+    """
     try:
         amount = float(amount)
         return amount
@@ -47,6 +63,9 @@ def check_amount(amount):
 
 
 def validate_currencies(currencies, currency_type='base currency'):
+    """
+    Function to get currency code input and validate
+    """
     msg = f"Enter {currency_type} ('c' to cancel): "
     currency = None
     while currency is None:
@@ -58,6 +77,9 @@ def validate_currencies(currencies, currency_type='base currency'):
 
 
 def validate_amount(base):
+    """
+    Function to get amount input and validate
+    """
     amount = None
     while amount is None:
         amount = input(f"Enter amount in {Colors.bold}{base}{Colors.reset} ('c' to cancel): ")
