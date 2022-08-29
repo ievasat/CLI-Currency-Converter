9ed9d75df8fdb307aadd5581fb6c85f82fb6f2ed
Ieva Sataite <ieva.sataite89@gmail.com>
2022-08-29 11:00:15 +0200

    documented main function

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import Colors
 
 
 def main():
+    """
+    Initialise program
+    """
     print(f'\n{Colors.label}Welcome to Currency Converter!{Colors.reset}')
     print()
     print(f'{Colors.text_bold}Commands:{Colors.reset}')
@@ -13,26 +16,42 @@ def main():
     print(f'{Colors.text_bold} * Q{Colors.reset}{Colors.text} - to quit.')
     print(f" * Enter currency {Colors.text_bold}code{Colors.reset}{Colors.text} to get it's name{Colors.reset}")
     currencies = get_currencies()
+
+    # running loop while command entered is not 'q'
     while True:
         command = input(f'\n{Colors.bold}Enter a command: {Colors.reset}').lower()
+
+        # check if command id currency code to get it's name
         if command.upper() in currencies:
             print(Colors.label + currencies[command.upper()]['description'] + Colors.reset)
+
+        # command to print all currencies and names
         elif command == 'list':
             print()
             print_currencies(currencies)
+
+        # command to convert or get rate
         elif command == 'rate' or command == 'convert':
             base = validate_currencies(currencies)
             if base:
                 currency = validate_currencies(currencies, currency_type='currency to convert to')
                 if currency:
+
+                    # if command is 'rate' just print rate
                     if command == 'rate':
                         get_exchange_rate_or_convert(base, currency)
+                    
+                    # if command is 'convert' - get amount and convert
                     else:
                         amount = validate_amount(base)
                         if amount:
                             get_exchange_rate_or_convert(base, currency, amount)
+        
+        # terminate loop
         elif command == 'q':
             break
+        
+        # if command is none of the above print error message
         else:
             print(f'{Colors.errors}Invalid command. Please try again.{Colors.reset}')
 
